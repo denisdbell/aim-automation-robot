29c848b013103c68c6e19f64953a29b6b4d7b103
Andrew Hall <ahall@medullan.com>
2015-03-24 15:46:13 -0500

    used requirements.txt for python dependencies

--- install.sh
+++ install.sh
@@ -2,19 +2,10 @@
 #curl- O https://www.python.org/ftp/python/2.7.6/python-2.7.6-macosx10.6.dmg
 #open python-2.7.6-macosx10.6.dmg
 
-echo “————— Downloading get-pip.py from gith...”
-curl -O  -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-echo “\n\n————— Executing get-pip.py...”
+echo "————— Downloading get-pip.py from internets..."
+#wget https://bootstrap.pypa.io/get-pip.py
+curl --silent --show-error --retry 5 -O https://bootstrap.pypa.io/get-pip.py
+echo "————— Executing get-pip.py..."
 python get-pip.py
-echo “\n\n————— Installing ‘robotframework’ via pip...”
-pip install robotframework
-echo “\n\n————— Installing ‘robotframework-selenium2library’ via pip...”
-pip install robotframework-selenium2library 
-echo “\n\n————— Installing ‘requests’ via pip...”
-pip install requests
-echo “\n\n————— Installing ‘robotframework-requests’ via pip...”
-pip install robotframework-requests
-echo “\n\n————— Installing ‘selenium’ via pip...”
-pip install selenium
-echo “\n\n————— Installing ‘vcrpy’ via pip...”
-pip install vcrpy
+echo "————— Installing python dependencies for robot automated tests..."
+pip install -r requirements.txt
